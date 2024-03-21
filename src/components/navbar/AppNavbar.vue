e5f7bb1228d3227cad666177eba31e21a599f2e1
kevinrodri11 <kevinrodriguez1209p@gmail.com>
2024-03-21 16:22:20 -0500

    cambiios

--- src/components/navbar/AppNavbar.vue
+++ src/components/navbar/AppNavbar.vue
@@ -25,7 +25,6 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useGlobalStore } from '../../stores/global-store'
-import AppNavbarActions from './components/AppNavbarActions.vue'
 import VuesticLogo from '../VuesticLogo.vue'
 
 defineProps({
