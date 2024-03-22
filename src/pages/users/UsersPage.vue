009948a5e39295a8a6dbf3675b8569b632382c97
kevinrodri11 <kevinrodriguez1209p@gmail.com>
2024-03-22 08:53:04 -0500

    Estado de cartera term

--- src/pages/users/UsersPage.vue
+++ src/pages/users/UsersPage.vue
@@ -1,131 +1,76 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import UsersTable from './widgets/UsersTable.vue'
-import EditUserForm from './widgets/EditUserForm.vue'
-import { User } from './types'
-import { useUsers } from './composables/useUsers'
-import { useModal, useToast } from 'vuestic-ui'
+<template>
+  <h1 class="page-title">Estado de cartera</h1>
+  <div class="card">
+    <div class="card-body">
+      <img src="../../../public/cyc_yambal.jpg" alt="Imagen tarjeta izquierda" class="img-fluid" />
+    </div>
 
-const doShowEditUserModal = ref(false)
+    <form class="formulario">
+      <h2 class="page-title">Ingrese el codigo de la directora</h2>
+      <input type="number" placeholder="Codigo directora" name="" />
+      <button>generar informe</button>
+    </form>
+  </div>
+</template>
 
-const { users, isLoading, filters, sorting, pagination, ...usersApi } = useUsers()
-
-const userToEdit = ref<User | null>(null)
-
-const showEditUserModal = (user: User) => {
-  userToEdit.value = user
-  doShowEditUserModal.value = true
-}
-
-const showAddUserModal = () => {
-  userToEdit.value = null
-  doShowEditUserModal.value = true
-}
-
-const { init: notify } = useToast()
-
-const onUserSaved = async (user: User) => {
-  if (userToEdit.value) {
-    await usersApi.update(user)
-    notify({
-      message: `${user.fullname} has been updated`,
-      color: 'success',
-    })
-  } else {
-    usersApi.add(user)
-    notify({
-      message: `${user.fullname} has been created`,
-      color: 'success',
-    })
-  }
-}
-
-const onUserDelete = async (user: User) => {
-  await usersApi.remove(user)
-  notify({
-    message: `${user.fullname} has been deleted`,
-    color: 'success',
-  })
-}
-
-const editFormRef = ref()
-
-const { confirm } = useModal()
-
-const beforeEditFormModalClose = async (hide: () => unknown) => {
-  if (editFormRef.value.isFormHasUnsavedChanges) {
-    const agreed = await confirm({
-      maxWidth: '380px',
-      message: 'Form has unsaved changes. Are you sure you want to close it?',
-      size: 'small',
-    })
-    if (agreed) {
-      hide()
-    }
-  } else {
-    hide()
-  }
-}
+<script>
+export default {}
 </script>
 
-<template>
-  <h1 class="page-title">Users</h1>
-
-  <VaCard>
-    <VaCardContent>
-      <div class="flex flex-col md:flex-row gap-2 mb-2 justify-between">
-        <div class="flex flex-col md:flex-row gap-2 justify-start">
-          <VaButtonToggle
-            v-model="filters.isActive"
-            color="background-element"
-            border-color="background-element"
-            :options="[
-              { label: 'Active', value: true },
-              { label: 'Inactive', value: false },
-            ]"
-          />
-          <VaInput v-model="filters.search" placeholder="Search">
-            <template #prependInner>
-              <VaIcon name="search" color="secondary" size="small" />
-            </template>
-          </VaInput>
-        </div>
-        <VaButton @click="showAddUserModal">Add User</VaButton>
-      </div>
-
-      <UsersTable
-        v-model:sort-by="sorting.sortBy"
-        v-model:sorting-order="sorting.sortingOrder"
-        :users="users"
-        :loading="isLoading"
-        :pagination="pagination"
-        @editUser="showEditUserModal"
-        @deleteUser="onUserDelete"
-      />
-    </VaCardContent>
-  </VaCard>
-
-  <VaModal
-    v-slot="{ cancel, ok }"
-    v-model="doShowEditUserModal"
-    size="small"
-    mobile-fullscreen
-    close-button
-    hide-default-actions
-    :before-cancel="beforeEditFormModalClose"
-  >
-    <h1 class="va-h5">{{ userToEdit ? 'Edit user' : 'Add user' }}</h1>
-    <EditUserForm
-      ref="editFormRef"
-      :user="userToEdit"
-      :save-button-label="userToEdit ? 'Save' : 'Add'"
-      @close="cancel"
-      @save="
-        (user) => {
-          onUserSaved(user)
-          ok()
-        }
-      "
-    />
-  </VaModal>
-</template>
+<style scoped>
+.card {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  justify-content: flex-end; /* Alinea el formulario a la derecha */
+}
+.card-body {
+  width: 35%;
+  float: none;
+  margin: 0;
+  padding: 10px;
+  border: 2px solid #ddd;
+  border-radius: 9px;
+  background-color: #fff;
+}
+.formulario {
+  width: 600px;
+  height: 440px;
+  margin-right: 100px;
+  padding: 50px;
+  border: 1px solid #ddd;
+  border-radius: 9px;
+  background-color: #fff;
+}
+h2 {
+  text-align: center;
+  margin-bottom: 30px;
+}
+input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  margin-bottom: 20px;
+}
+button {
+  margin-top: 0;
+  height: 47px;
+  padding: 10px 20px;
+  border: 1px solid #000;
+  border-radius: 3px;
+  background-color: #ffa05c;
+  color: #000;
+  cursor: pointer;
+}
+@media (max-width: 768px) {
+  /* Mismo estilo para pantallas pequeñas */
+  .contenedor,
+  .imagen-formulario,
+  .formulario {
+    width: 100%;
+    float: none;
+    margin: 0 auto;
+  }
+}
+</style>
